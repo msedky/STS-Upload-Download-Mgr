22a2123e9788a9f57f9482f0e40efdc1905ca4c1
msedky <m.hosamsedky@hotmail.com>
2023-07-15 13:48:21 +0300

    removed no longer needed files

--- db-scripts/sts_desgin_db_create_db_script.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE sts_design_db
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1
-    IS_TEMPLATE = False;
--- db-scripts/sts_desgin_db_drop_script.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE public."files";
-DROP TABLE public."item";
-DROP TABLE public."permissions";
-DROP TABLE public."permission_groups";
-
-DROP SEQUENCE public."permission_group_seq";
-DROP SEQUENCE public."permission_seq";
-DROP SEQUENCE public."item_seq";
-DROP SEQUENCE public."file_seq";
